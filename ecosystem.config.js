e5747147b60746d423a93604978032fbdc798d9d
Vishnu Sharma <vishnudbimcs@gmail.com>
2025-10-04 18:18:56 +0530

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'sunoo-backend-prod',
-      script: 'dist/main.js',
+      script: 'dist/src/main.js',
       instances: 'max',
       exec_mode: 'cluster',
       env: {
